0b3dfd28479d161ee48aa9b822abd6de46536019
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-04-02 16:18:16 +0900

    feat: FileDomainType 추가

--- backend/flowmeet-domain/src/main/java/kr/flowmeet/domain/file/entity/FileDomainType.java
+++ backend/flowmeet-domain/src/main/java/kr/flowmeet/domain/file/entity/FileDomainType.java
@@ -1,5 +1,9 @@
 package kr.flowmeet.domain.file.entity;
 
 public enum FileDomainType {
-    // 도메인에 맞게 값 추가 필요
+    NODE_ATTACHMENT, //노드 첨부파일
+    NODE_NOTE_IMAGE, //노드 노트 에디터 내 이미지
+    MEETING_SUMMARY, //회의 요약 관련 파일
+    CHAT_ATTACHMENT, //AI 채팅 첨부파일
+    TEMP //업로드 직후 임시 상태
 }
