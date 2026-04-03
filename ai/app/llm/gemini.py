35a37c966c617318de3389d7b0c0fdae985fc84f
ovepje0224 <ovepje2004@gmail.com>
2026-04-03 23:55:31 +0900

    getenv

--- ai/app/llm/gemini.py
+++ ai/app/llm/gemini.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBx1EjC0kq_YBTfpsb-nM_S2FjP9933rpU")
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def generate(system_prompt: str, user_input: str):
     prompt = f"""
