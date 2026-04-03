a45f843cc9038381ed82879c60c17f31e9a98eb4
J jet <ovepje2004@gmail.com>
2026-04-03 23:45:24 +0900

    remove

--- ai/app/llm/gemini.py
+++ ai/app/llm/gemini.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBx1EjC0kq_YBTfpsb-nM_S2FjP9933rpU")
+client = genai.Client(api_key="")
 
 def generate(system_prompt: str, user_input: str):
     prompt = f"""
